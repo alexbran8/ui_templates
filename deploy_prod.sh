0c67d4b09b54b81db57b7230418e88807da4ffa8
abran <alexandru.bran@nokia.com>
2021-05-27 10:03:41 +0300

    docker port update

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -14,4 +14,4 @@ docker stop nptjenkins && docker rm nptjenkins
 # delete existing image
 
 # run new image
-docker run  -d -p 5002:5000  --name nptjenkins nptjenkins
+docker run  -d -p 5002:8080  --name nptjenkins nptjenkins
