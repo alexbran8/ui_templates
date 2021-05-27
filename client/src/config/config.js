49d622586db57a43e7f9aae302f2ea6a5ba4a323
abran <alexandru.bran@nokia.com>
2021-05-27 21:58:30 +0300

    url update

--- client/src/config/config.js
+++ client/src/config/config.js
@@ -1,6 +1,6 @@
 const config = {
-  // apiUrl: 'https://apps.gdceur.eecloud.dynamic.nsn-net.net/nptbeta'
-  apiUrl: 'http://localhost:8080/nptbeta'
+  apiUrl: 'https://apps.gdceur.eecloud.dynamic.nsn-net.net/nptbeta'
+  // apiUrl: 'http://localhost:8080/nptbeta'
 }
 
 module.exports = config
