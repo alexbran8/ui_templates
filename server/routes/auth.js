250bebf34e00d145e7270b7ff22a9d7250638b78
abran <alexandru.bran@nokia.com>
2021-05-27 14:40:14 +0300

    req in prod

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -19,7 +19,8 @@ router.get('/logout', (req, res) => {
 router.get('/login-adfs', passport.authenticate('adfs', { session: false }) )
 
 router.get('/cbAdfs', passport.authenticate('adfs'), (req, res) => {
-  res.redirect('/npt/')
+  res.
+  res.redirect('/npt')
 })
 
 module.exports = router
