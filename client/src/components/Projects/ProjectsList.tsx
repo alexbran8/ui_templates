a8157983e08c633e66931167c845cb429b2ebac3
abran <alexandru.bran@nokia.com>
2021-10-13 13:58:37 +0300

    updates

--- client/src/components/Projects/ProjectsList.tsx
+++ client/src/components/Projects/ProjectsList.tsx
@@ -10,8 +10,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import { useTranslation } from "react-i18next";
 import { DataGrid } from '@mui/x-data-grid';
 import SimpleModal from "../common/Modal"
-import { useMutation, useQuery } from "@apollo/client";
-import gql from 'graphql-tag';
+import { useMutation, useQuery, gql } from "@apollo/client";
+// import gql from 'graphql-tag';
 import { useSelector } from "react-redux";
 
 const useStyles = makeStyles((theme) => ({
@@ -76,7 +76,6 @@ mutation ($data: Project) {
         message
       }
     }
-
 `;
 
 
