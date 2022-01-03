4433f633ebee2c9f5ff2fb3cfaaca1032475e7da
abran <alexandru.bran@nokia.com>
2022-01-03 10:04:09 +0200

    test#6

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ console.log('ENV',process.env.NODE_ENV)
 const envPath = process.env.NODE_ENV === 'development' ? '../.env' : '../../env/.env'
 require('dotenv').config({ path: envPath });
 
+
+
 const cookieSession = require("cookie-session");
 
 const bodyParser = require('body-parser')
